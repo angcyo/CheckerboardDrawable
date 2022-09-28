cb90977c2f093a3f3016d7ccfab7412e9f67c421
angcyo <angcyo@126.com>
2022-09-28 11:50:28 +0800

    * update androidx

--- app/src/main/java/top/defaults/checkerboarddrawable/view/MainActivity.java
+++ app/src/main/java/top/defaults/checkerboarddrawable/view/MainActivity.java
@@ -1,20 +1,25 @@
-package top.defaults.checkerboarddrawable;
+package top.defaults.checkerboarddrawable.view;
 
 import android.graphics.Color;
-import android.support.v7.app.AppCompatActivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
+
+import top.defaults.checkerboarddrawable.CheckerboardDrawable;
+
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+       /* setContentView(R.layout.activity_main);
         CheckerboardDrawable drawable = new CheckerboardDrawable.Builder()
                 .colorOdd(Color.LTGRAY)
                 .colorEven(Color.DKGRAY)
                 .size(20)
                 .build();
-        findViewById(android.R.id.content).setBackgroundDrawable(drawable);
+        findViewById(android.R.id.content).setBackgroundDrawable(drawable);*/
     }
 }
